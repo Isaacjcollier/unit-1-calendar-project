f8d6b4ee0064a299a61c7168f69ad9ed47525e7d
Isaacjcollier <isaacjcollier@gmail.com>
2016-08-10 14:43:52 -0600

    init

--- src/main.js
+++ src/main.js
@@ -11,54 +11,74 @@ $(document).ready(function() {
   // });
 
 
-
-
-
-
-		$('#calendar').fullCalendar({
-      googleCalendarApiKey: 'AIzaSyC8ideWt-GrCpNSt-AYa-ck0SQYDhdIAR8',
-      eventSources: [{
-          googleCalendarId: 'en.usa#holiday@group.v.calendar.google.com'
-      }],
-			header: {
-				left: 'prev,next today',
-				center: 'title',
-				right: 'month,agendaWeek,agendaDay'
-			},
-			selectable: true,
-			selectHelper: true,
-			select: function(start, end) {
-				var title = prompt('form');
-				var eventData;
-				if (title) {
-					eventData = {
-						title: title,
-						start: start,
-						end: end
-					};
-					$('#calendar').fullCalendar('renderEvent', eventData, true); // stick? = true
-				}
-				$('#calendar').fullCalendar('unselect');
-			},
-			editable: true,
-			eventLimit: true, // allow "more" link when too many events
-		});
-
-
-  $('form').on('submit', function (event) {
-    event.preventDefault()
-
-    let taskName = $('#task-name').val()
-    let taskDescription = $('#task-description').val()
-    let timeFrame = $('#time-frame').val()
-    // need to access the priority checkbox
-
-    console.log(taskName);
-    console.log(taskDescription);
-    console.log(timeFrame);
-
-    $($('[data-date="2016-08-09"]')[0]).append(taskName)
+  $($('#calendar').fullCalendar.select).on('click', function () {
+    console.log(this);
   })
+
+
+
+	$('#calendar').fullCalendar({
+    googleCalendarApiKey: 'AIzaSyDP1URPeSsp4BEcLUTg5SRMuK4vXDtKTmI',
+    eventSources: [{
+        googleCalendarId: 'en.usa#holiday@group.v.calendar.google.com'
+    }],
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay'
+		},
+		selectable: true,
+		selectHelper: true,
+		select: function(start, end) {
+      // console.log(this);
+      $('#the-form').css('visibility', 'visible')
+			$('#task-modal').modal('show');
+			var eventData;
+
+
+
+      $('#task-modal form').one('submit', function(e) {
+        e.preventDefault();
+        var title = $('#task-name').val();
+        var titleDescription = $('#task-description').val();
+        var titleTime = $('#time-frame').val()
+
+        console.log(title);
+        console.log(titleDescription);
+        console.log(titleTime);
+        var dateData = `${start.year()}-${start.month() + 1}-${start.date()}`;
+			  if (title) {
+  				eventData = {
+  					title: title,
+  					start: start,
+  					end: end
+  				};
+  				$('#calendar').fullCalendar('renderEvent', eventData, true);
+          $('#task-modal').modal('hide')
+        }
+        console.log($('[data-date="' + dateData + '"]').children())
+      })
+			$('#calendar').fullCalendar('unselect');
+		},
+		editable: true,
+		eventLimit: true, // allow "more" link when too many events
+	});
+
+
+  // $('form').on('submit', function (event) {
+  //   event.preventDefault()
+  //
+  //   let taskName = $('#task-name').val()
+  //   let taskDescription = $('#task-description').val()
+  //   let timeFrame = $('#time-frame').val()
+  //   // need to access the priority checkbox
+  //
+  //   console.log(taskName);
+  //   console.log(taskDescription);
+  //   console.log(timeFrame);
+  //
+  //   $($('[data-date="2016-08-09"]')[0]).append(taskName)
+  // })
   $.ajax({
     method: "GET",
 
