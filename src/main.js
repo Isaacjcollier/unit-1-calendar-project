6de7024e876fe374d1b2679163395a576056606b
Isaacjcollier <isaacjcollier@gmail.com>
2016-08-11 11:39:02 -0600

    Feature for Viewing a Created Task

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   console.log('sanity check');
+
   // calling the fullCalendar plugIn
 	$('#calendar').fullCalendar({
       // API call from googleCalendar to populate the fullCalendar plug-in with dates(holidays)
@@ -14,27 +15,32 @@ $(document).ready(function() {
 		},
 		selectable: true,
 		selectHelper: true,
+
     eventClick : function (event, jsEvent, view) {
-      // console.log(event.title);
-      // console.log(event.titleDescription);
-      // console.log(event.titleTime);
-      // $('#box-holder').append("<h1>" + event.title + "</h1>")
+      // empty the modal so no Duplication
+      $('.task-name').empty()
+      $('.task-description').empty()
+      $('.task-timeFrame').empty()
+      // Display Modal
+      $('#task-info').modal('show');
+      // Append all the information from the task into the modal frame
+      $('.task-name').append('<h6 style="color:black">' + event.title + '</h6>')
+      $('.task-description').append('<h6 style="color:black">' + event.titleDescription + '</h6>')
+      $('.task-timeFrame').append('<h6 style="color:black">' + event.titleTime + '</h6>')
     },
+    // when selecting an empty date, prompts the modal form
 		select: function(start, end) {
-      $('#the-form').css('visibility', 'visible')
 			$('#task-modal').modal('show');
 			var eventData;
-
+      //  when submiting the form, It will only create the event on that one day
       $('#task-modal form').one('submit', function(e) {
         e.preventDefault();
+        // establish variables for each form input
         var title = $('#task-name').val();
         var titleDescription = $('#task-description').val();
         var titleTime = $('#time-frame').val()
-
-        console.log(title);
-        console.log(titleDescription);
-        console.log(titleTime);
         var dateData = `${start.year()}-${start.month() + 1}-${start.date()}`;
+
 			  if (title) {
   				eventData = {
   					title: title,
